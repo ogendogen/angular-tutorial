03fb313d20dbe6ec9219f3a5cb2ad828c71abe68
ogendogen <kosmos500@interia.pl>
2021-01-08 22:36:11 +0100

    Refactor with shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { StarComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StarComponent
+  ],
   imports: [
+    CommonModule,
+  ],
+  exports: [
+    StarComponent,
+    FormsModule,
     CommonModule
   ]
 })
